9e691f1836692ad8108327bee38051f9016736af
oks404 <ksunya2004@list.ru>
2022-11-22 21:39:22 +1000

    Итоговый

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,58 @@ public class Main {
         else {
             System.out.println(year + " год не является високосным.");
         }
-
+//  Задание 4
+        int deliveryDistance = 95;
+        int timeDeliveryDays = 1;
+        if (deliveryDistance > 0 && deliveryDistance<20 ) {
+            System.out.println("Потребуется дней: " + timeDeliveryDays);
+        }
+        else {
+            timeDeliveryDays += (deliveryDistance +20) / 40;
+            System.out.println("Потребуется дней: " + timeDeliveryDays);
+        }
+//  Задание 5
+        int monthNumber = 1;
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Январь/Зима");
+                break;
+            case 2:
+                System.out.println("Февраль/Зима");
+                break;
+            case 3:
+                System.out.println("Март/Весна");
+                break;
+            case 4:
+                System.out.println("Апрель/Весна");
+                break;
+            case 5:
+                System.out.println("Май/Весна");
+                break;
+            case 6:
+                System.out.println("Июнь/Лето");
+                break;
+            case 7:
+                System.out.println("Июль/Лето");
+                break;
+            case 8:
+                System.out.println("Август/Лето");
+                break;
+            case 9:
+                System.out.println("Сентябрь/Осень");
+                break;
+            case 10:
+                System.out.println("Октябрь/Осень");
+                break;
+            case 11:
+                System.out.println("Ноябрь/Осень");
+                break;
+            case 12:
+                System.out.println("Декабрь/Зима");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+        }
 
 
 
